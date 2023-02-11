932aded75adf86275a9e4cdb0fd2e0f38738ee63
Evgeniy <zigzagmed@yandex.ru>
2023-02-12 01:02:57 +0300

    ДЗ к семинару 8

---
+++ task58/Program.cs
@@ -0,0 +1,73 @@
+﻿/*Задача 58: Задайте две матрицы. Напишите программу,
+которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+void FillArray(int[,] array)// метод для создания матрицы А и В
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 5);
+        }
+    }
+}
+
+void PrintArray(int[,] array)// метод для вывода матрицы в консоль
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// метод для умножения матриц:
+// А(0,0)*В(0,0) + А(0,1)*В(1,0) = М(0,0)
+// А(0,0)*В(0,1) + А(0,1)*В(1,1) = М(0,1)
+// А(1,0)*В(0,0) + А(1,1)*В(1,0) = М(1,0)
+// А(1,0)*В(0,1) + А(1,1)*В(1,1) = М(1,1)
+void Multiplication(int[,] array, int line, int colum, int[,] matrixA, int[,] matrixB)
+{
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < line; j++)
+        {
+            array[i, j] = 0;
+            for (int k = 0; k < colum; k++)
+            {
+                array[i, j] += matrixA[i, k] * matrixB[k, j];
+            }
+        }
+    }
+}
+
+
+Console.Write("Введите число строк матрицы: ");
+int line = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число столбцов матрицы: ");
+int colum = Convert.ToInt32(Console.ReadLine());
+int[,] matrixA = new int[line, colum];
+int[,] matrixB = new int[colum, line];
+FillArray(matrixA);
+FillArray(matrixB);
+int[,] matrixResult = new int[colum, line];
+Multiplication(matrixResult, line, colum, matrixA, matrixB);
+
+
+Console.Write("Матрица А");
+Console.WriteLine();
+PrintArray(matrixA);
+Console.WriteLine("Матрица В");
+PrintArray(matrixB);
+Console.WriteLine("Результирующая матрица");
+PrintArray(matrixResult);
---
+++ task62/Program.cs
@@ -0,0 +1,54 @@
+﻿/*Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07
+*/
+
+/* 
+        Метод для создания спирального массива.
+У меня сначала был очень длинный метод со множеством if и while 
+для каждой строки и столбца поочередно, потратил на эту задачу 2 дня
+но потом все сократил. Насколько же все было просто! Он может начинаться
+с любого числа и быть любого размера.
+ */
+void FillArray(int[,] spiral, int size, int num)
+{
+    int count = num;
+    int i = 0;
+    int j = 0;
+    while (count <= size * size + num - 1)
+    {
+        spiral[i, j] = count;
+        if (i <= j + 1 && i + j < size - 1) j++;
+        else if (i < j && i + j >= size - 1) i++;
+        else if (i >= j && i + j > size - 1) j--;
+        else i--;
+        count++;
+    }
+}
+
+// вывод массива в терминал
+void PrintArray(int[,] array, int size)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            // для выравнивания массива по столбцам
+            // все числа должны быть из 2-х символов: -3, 02 и т.д.
+            if (array[i, j] > (-1) && array[i, j] < 10) Console.Write("0");
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите размер спирального массива (для красоты число не должно быть больше 9):");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("С какой цифры начать спиральный массив? (для красоты от -9 до 10) ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[,] spiral = new int[size, size];
+FillArray(spiral, size, num);
+PrintArray(spiral, size);
